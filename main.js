383bbfd0b383c1f2153006f8c88252d7fe27223b
karim1safan <iibdae.almubtadi@gmail.com>
2026-02-04 13:07:52 +0200

    adding a new style and count tasks

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
 const inputBox = document.getElementById("input-box"); // Searching Box
 const listContainer = document.getElementById("list-container");
-
-const clickSound = document.getElementById("click-sound"); // Sound File
-const completedSound = document.getElementById("completed-sound");
-const removeSound = document.getElementById("deleted-sound")
 const addBtn = document.getElementById("add-btn");
 
-// Sound Effect When The User Input a New Task
-addBtn.addEventListener("click", () => clickSound.play());
+const countDisplay = document.querySelector(".count span");
 
+// Update task count
+function updateTaskCount() {
+  const totalTasks = document.querySelectorAll("li").length;
+  countDisplay.innerHTML = totalTasks;
+}
+
+// TODO: addTask Method
 function addTask() {
   if (inputBox.value === "") {
     alert("You must write something!");
   } else {
+    // make a new task
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+
+    // adding x icon
     let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    span.innerHTML = "\u00d7"; // x
     li.appendChild(span);
   }
   inputBox.value = "";
   saveDate();
+  updateTaskCount();
 }
 
 listContainer.addEventListener(
   "click",
-  function (e) {
+  (e) => {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-
-      if (e.target.classList.contains("checked")) {
-        completedSound.play();
-      }
 
       saveDate();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
-      removeSound.play();
       saveDate();
+      updateTaskCount();
     }
   },
-  false
+  false,
 );
 
 function saveDate() {
@@ -50,6 +52,7 @@ function saveDate() {
 
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
+  updateTaskCount();
 }
 
 showTask();
